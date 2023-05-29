614c7e4cc357303a1bf025b6ab294c028d1de991
Alejandro Chaves Luna <alejandrochalu_96@outlook.com>
2023-05-29 02:07:25 -0600

    .

--- Automatizacion/src/Prueba.java
+++ Automatizacion/src/Prueba.java
@@ -1,3 +1,5 @@
+import static org.junit.Assert.*;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -8,18 +10,39 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Prueba {
-    /*public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
 
-    }*/
     private WebDriver driver;
 
     @Before
 	public void setUp() throws Exception {
-		System.setProperty("webdriver.chrome.driver", "./src/test/resources/chromedriver/chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "C:/Users/aleja/OneDrive/Escritorio/Prueba/Automatizacion/src/Drivers/chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
-		driver.get("https://www.google.com");
+		driver.get("https://practice.automationtesting.in/my-account/");
+	}
+
+    @Test
+	public void testLogin() {
+		
+	    WebElement Usuario = driver.findElement(By.id("username"));
+		Usuario.clear();
+		Usuario.sendKeys("alejandrochalu96@gmail.com");
+
+        WebElement Contraseña = driver.findElement(By.id("password"));
+		Contraseña.clear();
+		Contraseña.sendKeys("Prueba_12345");
+
+        WebElement Login = driver.findElement(By.xpath("//*[@id='customer_login']/div[1]/form/p[3]/input[3]"));
+        Login.click();
+		
+
+		driver.manage().timeouts();
+		assertEquals("My Account – Automation Practice Site",driver.getTitle());
+	}
+
+    @After
+	public void tearDown() {
+		driver.quit();
 	}
 
 }
