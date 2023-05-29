ccb3fb898110c7d2cc0a00f7eed3d98390fdb7ac
Alejandro Chaves Luna <alejandrochalu_96@outlook.com>
2023-05-29 01:43:05 -0600

    Agrego Junit

---
+++ Automatizacion/src/Prueba.java
@@ -0,0 +1,25 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Prueba {
+    /*public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+    }*/
+    private WebDriver driver;
+
+    @Before
+	public void setUp() throws Exception {
+		System.setProperty("webdriver.chrome.driver", "./src/test/resources/chromedriver/chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com");
+	}
+
+}
